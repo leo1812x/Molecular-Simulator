957b5178ba91f30be90f947dca1d4b94aeca14c8
leo1812x <105297941+leo1812x@users.noreply.github.com>
2023-10-06 23:05:00 -0500

    re made the repo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Molecular Simulator
- to be made
+# Chemical simuator
+
+## Set up for delevopment
+Since i tend to forget how to locally run this project, these are the instructions to deploy locally with "hot reload"
+
+1. Run the "watch" comand from package.json
+   this conpiles the raw index.js to the bundled version with watchify
+
+2. run `tsc -w` to compile the index.ts file to index.js 
+
+3. host localy with 'Go live' extension in vscode
+
+
+
---
+++ index.ts
@@ -0,0 +1,252 @@
+//! Imports
+var pt = require('periodic-table');
+let {elements, symbols, numbers} = pt;
+
+//! Set up
+const SIMULATOR = document.getElementById('simulator');
+let objects: DOMElemento[] = [];
+
+//! Classes
+class DOMElemento {
+    static id:number = 0;
+    hold:boolean;
+    html:HTMLElement;
+    element;
+
+    //*for bonds
+    level:number;
+    letter:string;
+    electronInlevel:number;
+    max:number;
+    bonded:DOMElemento[] = [];
+
+    //* electrons
+    electrons: Record<number, HTMLDivElement | undefined> = {
+        1:undefined,
+        2:undefined,
+        4:undefined,
+        5:undefined,
+        6:undefined,
+        7:undefined,
+        8:undefined,
+    }
+
+    constructor(element) {
+        this.element = window.structuredClone(element);
+
+        //* create html
+        this.html = document.createElement('div');
+        this.html.innerHTML = `${element.name}`;
+
+        //* set attributes
+        this.html.setAttribute('id', DOMElemento.id.toString());
+        DOMElemento.id++;
+        this.html.setAttribute('class', 'element');
+        
+        //* Event listeners
+        this.html.onpointerdown = () =>{
+            this.hold = true;
+            this.html.style.left = '0%';
+            this.html.style.top = '0%;';
+            
+        }
+        this.html.onmousemove = (e) => {
+            e.preventDefault();                                                
+            if(this.hold){
+                this.move(e);
+                this.collisionCheck();
+            }
+        }
+        this.html.onpointerup = () => {
+            this.hold = false;
+        }
+        this.html.onmouseout = (e) =>{
+            if (this.hold){
+                this.move(e);
+                this.collisionCheck();
+            }
+        }
+
+        
+        //* append to simulator and array
+        this.initialStability();
+        SIMULATOR.append(this.html);
+        objects.push(this);
+        
+        //*add electrons
+        this.lewisInit();
+    }
+
+    //! Lewis structure
+    lewisInit(){
+        console.log(this.electronInlevel);
+        
+        for (let i = 0; i < this.electronInlevel; i++){
+            //* create html element and atributes    
+            let dot = document.createElement('div');
+            dot.setAttribute('class', 'electron');
+            dot.setAttribute('id', 'e' +(i + 1));
+            // dot.innerHTML = 'e-';
+            dot.style.fontSize = '12px';
+            dot.classList.add()
+
+            //* append to its element and organize them in electrons[]
+            this.html.append(dot);
+            this.electrons[i] = dot;
+        } 
+    }
+
+    //! class functions
+    move(e):void {
+        //*this peace of code gives the coordenates of the click relative to the SIMULATOR
+        let rect = SIMULATOR.getBoundingClientRect();
+        let x = e.clientX - rect.left; 
+        let y = e.clientY - rect.top;  
+
+        //*make move
+        this.html.style.left = x - 50 + 'px';
+        this.html.style.top = y - 50 + 'px'; 
+
+        //* keep inside box
+        if (x <= 62) this.html.style.left = '12px'; 
+        if (y <=62) this.html.style.top = '13px'; 
+        if (x >= rect.right - 74) this.html.style.left = (rect.right - 126) + 'px'; 
+        if (y >= rect.bottom - 135) this.html.style.top = (rect.bottom - 185) + 'px'; 
+    }
+
+    collisionCheck():void {
+        //*get circle coordenates
+        let current = this.html.getBoundingClientRect();
+        
+        //*check each element avoiding current
+        objects.forEach((other, index) =>{
+
+            if (other.html !== this.html){
+                //* check other circle coordenates
+                let otherx = other.html.getBoundingClientRect().x;
+                let othery = other.html.getBoundingClientRect().y;
+
+                //* distance formula => collision
+                let distance = Math.sqrt(Math.pow(otherx - current.x, 2) + Math.pow(othery - current.y, 2))
+                // console.log(distance);
+                
+                //* crash
+                if (distance < 100){
+                    // console.log(`crashed with ${other.element.name}`);
+                    this.Bond(other);
+                }
+            }
+        })
+    }
+
+    //! CHIMESTRY MATH
+    initialStability(){
+        let {electronicConfiguration} = this.element;
+
+        //*getters
+        this.level = electronicConfiguration[electronicConfiguration.length - 3];
+        this.letter = electronicConfiguration[electronicConfiguration.length - 2];
+        this.electronInlevel = +electronicConfiguration[electronicConfiguration.length - 1];        
+
+        //*fix elenctronInlevels
+        if (this.letter === 'p') {this.electronInlevel += 2;}
+        if (this.letter === 'd') {this.electronInlevel += 8;}
+        if (this.letter === 'f') {this.electronInlevel += 19;}
+
+        // console.log(electronicConfiguration);
+        // console.log(`level = ${this.level}`);
+        // console.log(`letter = ${this.letter}`);
+        // console.log(`electron in level = ${this.electronInlevel}`);
+        
+        this.isStable();
+    }
+
+    isStable(){
+        //* get max num in last ring
+        //? Change this for something better?
+        if (this.level == 1) {this.max = 2;}
+        if (this.level == 2) {
+            if (this.letter == 's'){this.max = 2}
+            else if (this.letter == 'p'){this.max = 8}}
+        if (this.level == 3) {
+            if (this.letter == 's'){this.max = 2;}
+            else if (this.letter == 'p'){this.max = 8}
+            else if (this.letter == 'd'){this.max = 18}}
+        if (this.level == 4) {
+            if (this.letter == 's'){this.max = 2;}
+            else if (this.letter == 'p'){this.max = 8}
+            else if (this.letter == 'd'){this.max = 18}
+            else if (this.letter == 'f'){this.max = 32}}
+        if (this.level == 5) {
+            if (this.letter == 's'){this.max = 2;}
+            else if (this.letter == 'p'){this.max = 8}
+            else if (this.letter == 'd'){this.max = 18}
+            else if (this.letter == 'f'){this.max = 32}}
+        if (this.level == 6) {
+            if (this.letter == 's'){this.max = 2;}
+            else if (this.letter == 'p'){this.max = 8}
+            else if (this.letter == 'd'){this.max = 18}
+            else if (this.letter == 'f'){this.max = 32}}
+        if (this.level == 7) {
+            if (this.letter == 's'){this.max = 2}
+            else if (this.letter == 'p'){this.max = 8}
+            else if (this.letter == 'd'){this.max = 18}}
+
+
+        //*turn green if stable
+        if (this.electronInlevel === 0 ||
+            this.electronInlevel === this.max){
+            this.html.style.backgroundColor = 'green';}
+
+        //*turn red if inestable
+        else {this.html.style.backgroundColor = 'red';}
+
+        // console.log(`level:${this.level}---max:${this.max}---current:${this.electronInlevel}`);
+    }
+
+    Bond(other:DOMElemento){
+        //! Ion bond
+        //* check electronegativity
+        if (Math.abs(this.element.electronegativity - other.element.electronegativity) > 1.7){
+            let moreNegative = (this.element.electronegativity > other.element.electronegativity) ? this : other;
+            let lessNegative = (this.element.electronegativity < other.element.electronegativity) ? this : other;
+            console.log('Ionic bond!');
+            
+
+            moreNegative.electronInlevel += 1;
+            lessNegative.electronInlevel -= 1;
+            
+        }
+
+        //! covalent bond
+        else if (Math.abs(this.element.electronegativity - other.element.electronegativity) < 1.7){
+            console.log('covalent bond!');
+            console.log(other);
+            
+            this.bonded.push(other);
+            
+            
+        }
+
+        this.isStable();
+    }
+}
+
+class DOMCompound{
+    constructor(...element:DOMElemento[] | DOMCompound[]){
+        elements
+        
+    }
+}
+
+//! Testing
+// let circle = new DOMElemento(elements.Helium);
+// let circle3 = new DOMElemento(numbers[2]);
+// let circle4 = new DOMElemento(numbers[3]);
+let circle5 = new DOMElemento(symbols.O);
+let circle2 = new DOMElemento(symbols.N);
+
+circle2.html.style.left = '200px';
+
+
+
