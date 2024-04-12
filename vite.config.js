6ee379bc8595428819d9020cc163631d3091d4ee
leo1812x <leo1812x@gmail.com>
2024-04-12 14:23:51 -0500

    and again :(

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 
 // vite.config.js
 export default {
-    base : '/<https://github.com/leo1812x/Molecular-Simulator>/',
+    base: '/Molecular-Simulator/',
     build: {
       outDir: 'docs'
     }
